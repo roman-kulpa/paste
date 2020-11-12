7d7ee35f1f16078a04e2d7d8011c838324383f34
richbachman <rb@richbachman.com>
2020-11-12 16:50:40 -0700

    feat(website): build popular components and patterns section

--- packages/paste-website/src/pages/new.tsx
+++ packages/paste-website/src/pages/new.tsx
@@ -3,6 +3,7 @@ import {Helmet} from 'react-helmet';
 import {Box} from '@twilio-paste/core/box';
 import {SiteWrapper} from '../components/site-wrapper-new';
 import {SiteMetaDefaults} from '../constants';
+import {ComponentsAndPatterns} from '../components/homepage/ComponentsAndPatterns';
 
 const Homepage: React.FC<{}> = (): React.ReactElement => {
   return (
@@ -12,7 +13,9 @@ const Homepage: React.FC<{}> = (): React.ReactElement => {
         <link rel="canonical" href="https://paste.twilio.design" />
         <meta name="description" content={SiteMetaDefaults.DESCRIPTION} />
       </Helmet>
-      <Box maxWidth="size120">test</Box>
+      <Box>
+        <ComponentsAndPatterns />
+      </Box>
     </SiteWrapper>
   );
 };
